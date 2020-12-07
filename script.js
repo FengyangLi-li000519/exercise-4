c2149718c6be60467ca485e0bb34514df4e5130b
leon2200 <li000519#algonquinlive.com>
2020-12-06 16:47:20 -0800

    complete assignment

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+
+const $button = document.getElementById('button')
+const $menu = document.getElementById('menu')
+
+let menuToggle = false
+
+$button.addEventListener('click',function(){
+     if (menuToggle === false){
+         $menu.style.display = 'block'
+        
+         menuToggle = true
+     }else if(menuToggle === true){
+        $menu.style.display = 'none'
+         menuToggle = false
+     }
+})
+window.addEventListener('resize',function(){
+    if(window.innerWidth>=768){
+        $menu.style.display = 'none'
+        
+        menuToggle = false
+    }
+})
 AOS.init()
